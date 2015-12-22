084b8e258d70a29b0d77dca563611b224e6dc261
Toshiyuki Masui <masui@pitecan.com>
2015-12-22 12:38:47 +0900

    ...

--- lib/gyazo/client.rb
+++ lib/gyazo/client.rb
@@ -4,19 +4,17 @@ module Gyazo
 
     attr_accessor :id, :user_id, :user_agent, :host
 
-    def initialize(access_token = null)
+    def initialize(access_token = nil)
       @access_token = access_token
       @user_agent = "GyazoRubyGem/#{Gyazo::VERSION}"
     end
-
-    def upload(imagefile,time=null)
+    
+    def upload(imagefile,time=nil)
       url = "https://upload.gyazo.com/api/upload"
-      time = Time.now unless time
       res = HTTMultiParty.post url, {
         :query => {
           :access_token => @access_token,
           :imagedata => File.open(imagefile)
-          :created_at => time.to_i
         },
         :header => {
           'User-Agent' => @user_agent
@@ -24,6 +22,34 @@ module Gyazo
       }
       raise Gyazo::Error, res.body unless res.code == 200
       return JSON.parse res.body
+    end
+
+    def upload_new(imagefile,time=nil)
+      url = "https://upload.gyazo.com/upload.cgi"
+      time = Time.now unless time
+      res = HTTMultiParty.post url, {
+        :query => {
+          :access_token => @access_token,
+          :imagedata => File.open(imagefile),
+          :created_at => time.to_i * 1000
+        },
+        :header => {
+          'User-Agent' => @user_agent
+        }
+      }
+      puts res
+      raise Gyazo::Error, res.body unless res.code == 200
+      url = res.body
+      url =~ /[0-9a-f]{32}/
+      id = $&
+      puts url
+      puts id
+      { # upload.cgiを使った場合、無理矢理JSONぽいのを生成する
+        "image_id" => id,
+        "permalink_url" => "http://gyazo.com/#{id}",
+        "url" => "https://i.gyazo.com/#{id}.png",
+        "type" => "png"
+      }
     end
 
     def list(query = {})
